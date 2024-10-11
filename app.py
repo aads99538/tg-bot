5743fcda4db7daee95923ded53395fe72547be04
aads99538 <aads99538@gmail.com>
2024-10-11 04:49:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import time
 import telegram
 
 # Токен бота
-bot_token = '7764076864:AAF-0KuyEufJIS7mC6u1nT2ZwA0YXKu>
+bot_token = '7764076864:AAF-0KuyEufJIS7mC6u1nT2ZwA0YXKuYlhY'
 
 # ID канала
 channel_id = -1001746118789
@@ -24,7 +24,7 @@ def update_dots():
     dots += "."
     if len(dots) > 5:
       dots = "."
-    time.sleep(0.1)
+    time.sleep(0.1) # Задержка 0.1 секунды
 
 # Получаем итератор для обновления точек
 dots_iterator = update_dots()
@@ -38,7 +38,7 @@ while True:
     # Проверяем, отправлено ли сообщение ранее
     if last_message_id is None:
       # Отправляем сообщение в канал
-      last_message = bot.send_message(chat_id=channel_i>
+      last_message = bot.send_message(chat_id=channel_id, text=f"{message_text}{dots}")
       last_message_id = last_message.message_id
 
     else:
@@ -48,3 +48,9 @@ while True:
         message_id=last_message_id,
         text=f"{message_text}{dots}"
       )
+
+    # Пауза между отправками (0.1 секунды)
+    time.sleep(0.1)
+  except Exception as e:
+    print(f"Ошибка: {e}")
+    time.sleep(5) # Пауза в случае ошибки
